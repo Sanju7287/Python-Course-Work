a57a6a1d2e2dff6f9a68e14b668067c9e4d067f9
Sanju7287 <chandrasanjaykumar01@gmail.com>
2025-07-02 05:50:22 +0000

    Classwork

---
+++ Classwork/4.pythonOperators.py
@@ -0,0 +1,10 @@
+#1. Arithmetic Operators
+a=20
+b=10
+print("Addition(+):",a+b) #Addition(+): 30
+print("Subtraction(-):",a-b) #Subtraction(-): 10
+print("Multiplication(*):",a*b) #Multiplication(*): 200
+print("Division(/):",a/b) #Division(/): 2.0
+print("Floor Division(//):",a//b) #Floor Division(//): 2
+print("Modulus(%):",a%b) #Modulus(%): 0
+print("Exponentiation(**):",a**b) #Exponentiation(**): 10240000000000
