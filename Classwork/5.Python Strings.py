1ced8a0fcd6a4f9f1e7ca3f07172714b9ef01591
Sanju7287 <chandrasanjaykumar01@gmail.com>
2025-07-05 05:39:36 +0000

    Classwork

--- Classwork/5.Python Strings.py
+++ Classwork/5.Python Strings.py
@@ -60,3 +60,42 @@ print("hello".rfind("l")) # Output: 3 (rfind() method)
 print("hello".index("e")) # Output: 1 (index() method)
 print("hello".rindex("l")) # Output: 3 (rindex() method)
 print("friends".count("r")) # Output: 1 (count() method)
+
+#6.String Testing Methods (Boolean Results)
+print("vikash".startswith("vik")) #Output: True (startswith() method)
+print("Hemanth".endswith("th")) #Output: True (endswith() method)
+print("Friends12".isalpha()) #Output: False (isalpha() method)
+print("Vikash123".isalnum()) #Output: True (isalnum() method)
+print("sanjay".islower()) #Output: True (islowe() method)
+print("HEMANTH".isupper()) #Output: True (isupper() method)
+print("Hello Friends".istitle()) #Output: True (istitle() method)
+print("Vikash123".isidentifier()) #Output: True (isidentifier() method)
+print("Vikash123".isdecimal()) #Output: False (isdecimal() method)
+print("716264706".isdigit()) #Output: True (isdigit() method)
+print("Madhav".isnumeric()) #Output: False (isnumeric() method)
+
+#7.Replace & Modify Methods
+print("madhav".replace("m","d")) #Output: dadhav (replace() method)
+print("sheshu".translate(str.maketrans("h","u"))) #Output: suesuu (translate() method)
+print("gopi".maketrans("op","@%")) #Output: {111: 64, 112: 37} (maketrans() method)
+
+
+#6. Splitting & Joining Methods
+text1="sanjay"
+print(text1.split(" "))#Splits the string into a list by sep
+print(text1.rsplit(" "))# Splits from the right side.
+print("text1".splitlines(" "))#Splits at line breaks ( ).
+result=' '.join(text1)# Joins elements with a separator.
+print(result)
+text2="hello-world"#Splits into a 3-part tuple at first sep.
+print(text2.partition("-"))
+print(text2.rpartition("-"))# Splits into a3-part tuple at last sep.
+
+#7. Whitespace & Trimming Methods
+print("  hello ".strip()) #Removes leading and trailing characters (default: spaces).
+print("----hello".lstrip("-"))#Removes leading characters.
+print("hello----".rstrip("-")) #Removes trailing characters.
+
+#10.Encoding & Decoding Methods
+print("vikash".encode("utf-7")) #Output: b'vikash'
+print(b'vikash'.decode("utf-7")) #Output: vikash
