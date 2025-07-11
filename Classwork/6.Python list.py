d7e8d79fd5fbda7f47c3cfe480b50e24cff50267
Sanju7287 <chandrasanjaykumar01@gmail.com>
2025-07-11 05:37:50 +0000

    Classwork

---
+++ Classwork/6.Python list.py
@@ -0,0 +1,33 @@
+#List
+a=[2,5,3]
+print(a)
+
+#List with Elements
+a=[1,2,3,4,5]
+courses=["DS","DA","Python"]
+l=list([2,2.6,'vikash',True])
+
+#List with nested lists
+l=[[1,2,3,4],["vikasj","hemanth","sheshu","mani"]]
+
+#Using Indexing (Positive & Negative)
+a=["vikasj","hemanth","sheshu","mani"]
+print(a[0])
+print(a[-2])
+
+#Using Slicing
+l=[12,45,36,98,101]
+print(a[1:3])
+print(a[:4])
+print(a[-3:-1])
+
+#Changing Elements
+n=[10,20,30,40]
+n[2]=100
+print(n)
+
+#Adding Elements
+n.append(50)
+n.extend([78,83,99])
+n.insert(1,5)
+print(n)
